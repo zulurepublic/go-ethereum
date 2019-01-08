05848f6e580e3576b112d6985e01604373f0a2c9
Timo Hedke <lucky_expert@t-online.de>
2019-01-08 10:54:15 -0300

    Changed to use zulu docker hub

--- cmd/puppeth/module_node.go
+++ cmd/puppeth/module_node.go
@@ -32,7 +32,7 @@ import (
 
 // nodeDockerfile is the Dockerfile required to run an Ethereum node.
 var nodeDockerfile = `
-FROM ethereum/client-go:latest
+FROM luckyexpert/zulu-go-ethereum:latest
 
 ADD genesis.json /genesis.json
 {{if .Unlock}}
